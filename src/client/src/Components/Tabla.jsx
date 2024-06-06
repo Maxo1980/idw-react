4d0394ee4fb8a6cfa6f412240a5b692bd44f5919
Maximiliano Kildoff <maxi.kildoff@gmail.com>
2024-06-06 20:50:47 -0300

    Se agrega funcionalidad de borrar datos + cuadro de dialogo  para confirmar operacion

--- src/client/src/Components/Tabla.jsx
+++ src/client/src/Components/Tabla.jsx
@@ -1,35 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Tabla.css'
 
 const Tabla = () => {
 
   const [tabla, setTabla] = useState([])
-  const [del, setDel] = useState('')
+  
   
   //Traer data desde la API
   
   const URLDATA =  'http://localhost:3001/tiposAlojamiento/getTiposAlojamiento'
-  const URLDELETE = 'http://localhost:3001/tiposAlojamiento/deleteTipoAlojamiento/:id'
+  const URLDELETE = 'http://localhost:3001/tiposAlojamiento/deleteTipoAlojamiento/'
 
   //BUSCAR Y MOSTRAR DATOS DE LA BD
-  const showData = async () => {
-    const response = await fetch(URLDATA)
-    const data = await response.json()
-    setTabla(data)
-  }
-   showData()
-
+  useEffect(() => {
+    
+      const showData = async () => {
+        try{
+          const response = await fetch(URLDATA)
+          const data = await response.json()
+          setTabla(data)
+        }catch(error){
+          console.log('No de pudo acceder a la DATA', error)
+        }
+      }
+    showData();
+  }, [])
+ 
    //BORRAR DATOS
 
    const borrarDatos = async (dato) => {
-    console.log(dato)
-    // const reponse = await fetch(URLDELETE+dato, {
-    //   method: 'DELETE'
-    // })
-    // const data = await await reponse.json()
-    // setDel(data)      
-   }
-   
+     alertify.confirm('Se borrar el registro Nro: '+dato, 'Esta operación no se puede deshacer. ¿Desea continuar?', function(){
+        fetch(URLDELETE+dato, {
+          method: 'DELETE' 
+          })},
+          function(){ alertify.error('Operación cancelada!')});
+
+
+    
+    
+        
+      }
    const editarDatos = async (dato) => {
     console.log(dato)
    }
